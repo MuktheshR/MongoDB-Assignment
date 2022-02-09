2e900d8ec0f92fe3c591880c9e6fec5aa11edd82
MuktheshR <radhamukthesh1@gmail.com>
2022-02-09 22:22:29 +0530

    Mongodb commands in JS

--- commands.js
+++ commands.js
@@ -7,7 +7,7 @@ Q6. db.movies.find({$or:[{year:{$lte:2000}}, {year:{$gte:2010}}]})
 
 U1. db.movies.update({title: "The Hobbit: An Unexpected Journey"}, {$set: {synopsis: "A reluctant hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home - and the gold within it - from the dragon Smaug."}})
 U2. db.movies.update({title: "The Hobbit: The Desolation of Smaug"}, {$set: {synopsis: "The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim Erebor, their homeland, from Smaug. Bilbo Baggins is in possession of a mysterious and magical ring."}})
-U3: db.movies.update({title: "Pulp Fiction"}, {$push: {actors: "Samuel L. Jackson"}})
+U3: db.movies.update({title: "Pulp Fiction"}, {$set: {actors: "Samuel L. Jackson"}})
 
 TS1: db.movies.find({synopsis: /.Bilbo./})
 TS2: db.movies.find({synopsis: /.Gandalf./})
